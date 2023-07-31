682646eec912c0687dad34ecbb2c40b345c225f8
jinsu <realwaterkim@datacommand.kr>
2023-07-31 12:56:53 +0900

    add node for exporting

---
+++ node.go
@@ -0,0 +1,10 @@
+package jason
+
+type Node interface {
+	Path(path string) []Node
+	Get(key string) (Node, error)
+	List() []Node
+	String() string
+	Number() (float64, error)
+	Bool() (bool, error)
+}
